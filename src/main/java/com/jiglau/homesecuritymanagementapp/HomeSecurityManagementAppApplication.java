d4dbbeeaa97338cd61356e533a167de44843dfd9
Mihai Jiglau <jiglaumihai@gmail.com>
2020-11-19 01:50:59 +0200

    [IMPLEMENT] First commit [check info] - Added main Java class - Added pom.xml dependencies

---
+++ src/main/java/com/jiglau/homesecuritymanagementapp/HomeSecurityManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.jiglau.homesecuritymanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSecurityManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeSecurityManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiglau/homesecuritymanagementapp/HomeSecurityManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiglau.homesecuritymanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSecurityManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
